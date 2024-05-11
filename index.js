bc0c7124a8d09ef34a7f8ad20f360b1cefcf2dab
mateusbarbosa <barbosamat16@gmail.com>
2024-05-11 14:46:53 -0300

    Sync: sync files

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+require("./src/routes/user.routes.js")(app);
+
 app.listen(port, hostname, () => {
   console.log(`Starting server at ${hostname}:${port}`);
 });
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const db = require("../models");
+
+const User = db.users;
+
+exports.get = (req, res) => {};
