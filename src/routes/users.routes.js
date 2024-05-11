5887503a97d3e3c94b8d0d6ff0a8028b4eefb3ac
mateusbarbosa <barbosamat16@gmail.com>
2024-05-11 18:49:41 -0300

    Chore: Remove unecessary headers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,10 +5,7 @@ const controller = require("../controllers/users.controller.js");
  */
 module.exports = (app) => {
   app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     next();
   });
 
